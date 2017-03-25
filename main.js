6830e7e54f82337bc22d5fc0cd815a5d9eeb1b87
ktordoff13 <ktordoff13@gmail.com>
2017-03-24 21:00:54 -0500

    updated to add a data source and fetch data

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ListView } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -33,8 +33,29 @@ class ArticlesScreen extends React.Component {
   static navigationOptions = {
     title: 'Articles',
   };
+
+  constructor(props) {
+      super(props)
+      this.state = {
+        hasFetched: false,
+        dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+      };
+    }
+
+  componentDidMount(){
+    makeFetch('https://jsonplaceholder.typicode.com/posts').then((data) => {
+      this.setState(()=> {
+        return {
+          hasFetched: true,
+          dataSource: this.state.dataSource.cloneWithRows(data)
+        };
+      })
+    })
+  }
+
   render() {
     const { navigate } = this.props.navigation;
+    const { hasFetched, dataSource } = this.state;
     return (
       <View style={styles.container}>
         <Text>Articles</Text>
@@ -42,6 +63,13 @@ class ArticlesScreen extends React.Component {
           onPress={() => navigate('Home')}
           title="Home"
         />
+        {hasFetched ?
+          <ListView
+              dataSource={dataSource}
+              renderRow={(rowData) => <Text>{rowData.title}</Text>}
+            />
+            : <Loader />
+        }
       </View>
     );
   }
@@ -61,5 +89,26 @@ const Routes = StackNavigator({
   Home: { screen: HomeScreen },
   Articles: { screen: ArticlesScreen },
 });
+
+//****this is for fetching data*******************************************************
+
+const checkStatus = (response) => {
+  if (!response.ok) { // status in the range 200-299 or not
+    return Promise.reject(new Error(response.statusText || 'Status not OK'))
+  }
+  return response
+}
+
+const parseJSON = (response) => response.json()
+
+const makeFetch = (url, options) => fetch(url, options)
+  .then(checkStatus)
+  .then(parseJSON)
+
+class Loader extends React.Component {
+  render() {
+    return (<Text>Loading...</Text>);
+  }
+}
 
 Expo.registerRootComponent(App);
